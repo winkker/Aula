ceaa75833b97e727613d3e20ada458a616b31d6e
winkker <winkker@gmail.com>
2022-08-16 12:18:01 -0400

    ProdutoTeste update to be more like the teacher  exemple

--- src/Classes_objetos/ProdutoTeste.java
+++ src/Classes_objetos/ProdutoTeste.java
@@ -8,7 +8,18 @@ public class ProdutoTeste {
         produto.preco = 2239.99f;
         produto.desconto = 0.95f;
 
+        var produto2 = new Produto();
+        produto2.nome = "Caneta preta";
+        produto2.preco = 12.56;
+        produto2.desconto = 0.99;
+
+
         System.out.printf("O %s custa R$ %.2f com o desconto" +
                 "",produto.nome, (produto.preco * produto.desconto));
+
+
+
+
+
     }
 }
