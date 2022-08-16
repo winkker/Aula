9a1120eabcf4fd52f9904cbab1494b92feebfcfa
winkker <winkker@gmail.com>
2022-08-16 13:41:10 -0400

    16/08 estudos aula 96

--- src/Classes_objetos/ProdutoTeste.java
+++ src/Classes_objetos/ProdutoTeste.java
@@ -6,17 +6,18 @@ public class ProdutoTeste {
         Produto produto = new Produto();
         produto.nome = "laptop";
         produto.preco = 2239.99f;
-        produto.desconto = 0.95f;
+        produto.desconto = 0.05f;
 
         var produto2 = new Produto();
         produto2.nome = "Caneta preta";
         produto2.preco = 12.56;
-        produto2.desconto = 0.99;
+        produto2.desconto = 0.01;
 
 
         System.out.printf("O %s custa R$ %.2f com o desconto" +
-                "",produto.nome, (produto.preco * produto.desconto));
-
+                "",produto.nome, produto.precoComDesconto());
+        System.out.printf("O %s custa R$ %.2f com o desconto" +
+                "",produto2.nome, produto2.precoComDesconto());
 
 
 
